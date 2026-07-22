30585802198b8f0c70634cb3c792474dcedc16f4
Arjun <shivvilonsolutions@gmail.com>
2026-07-22 21:53:36 +0530

    Fix typo in backend URL (hardware not oardware)

--- manufacturing/src/api/api.js
+++ manufacturing/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ardware-erp-backend.onrender.com/api",
+  baseURL: "https://hardware-erp-backend.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },

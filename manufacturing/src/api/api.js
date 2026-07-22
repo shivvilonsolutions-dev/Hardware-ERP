595828d6953e9c98af1e673c24fd1615f8276470
Arjun <shivvilonsolutions@gmail.com>
2026-07-22 21:52:04 +0530

    Update API URL to new backend

--- manufacturing/src/api/api.js
+++ manufacturing/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://shivvilon-solution-manufacturing-project.onrender.com/api",
+  baseURL: "https://ardware-erp-backend.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },

c91edc5b2e2e9e585fecb621a7545e6525e66498
Arjun <shivvilonsolutions@gmail.com>
2026-07-23 10:50:36 +0530

    Update frontend API URL to correct backend URL

--- manufacturing/src/api/api.js
+++ manufacturing/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://oardware-erp-backend.onrender.com/api",
+  baseURL: "https://hardware-erp-backend.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
